10b4c7702c1bf3909d2f2b5de349ab803a8d0e30
Oleksandr Iadvigun <iadvigun@gmail.com>
2021-07-16 18:15:11 +0300

    init commit

---
+++ src/test/kotlin/sigma/software/leovegas/drugstore/DrugstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package sigma.software.leovegas.drugstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DrugstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
