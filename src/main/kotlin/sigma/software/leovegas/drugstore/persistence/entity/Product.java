fc3112346ec6d3b4dd6a1ae3e9608d6fbf8fe888
Volodymyr Zabolotnyi <vov4ikf97@gmail.com>
2021-07-19 13:42:42 +0000

    8: Persitence layer for Product

---
+++ src/main/kotlin/sigma/software/leovegas/drugstore/persistence/entity/Product.java
@@ -0,0 +1,30 @@
+package sigma.software.leovegas.drugstore.persistence.entity;
+
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "price")
+    private BigDecimal price;
+}
